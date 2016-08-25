ddcef5dc5d34ff1782647b2f700d4d489d6e46fc
mehanik6 <mehanik67@dbk.ru>
2016-08-25 21:02:21 +0300

    you commit

--- device_hit.mk
+++ device_hit.mk
@@ -49,14 +49,20 @@ PRODUCT_COPY_FILES += \
     device/digma/hit/rootdir/configs/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
 
 PRODUCT_COPY_FILES += \
-    device/digma/hit/rootdir/root/sbin/busybox:root/sbin/busybox \
-    device/digma/hit/rootdir/root/fstab.mt6582:root/fstab.mt6582 \
-    device/digma/hit/rootdir/root/init.recovery.mt6582.rc:root/init.recovery.mt6582.rc \
-    device/digma/hit/rootdir/root/init.rc:root/init.rc \
-    device/digma/hit/rootdir/root/init.mt6582.rc:root/init.mt6582.rc \
-    device/digma/hit/rootdir/root/init.modem.rc:root/init.modem.rc \
-    device/digma/hit/rootdir/root/ueventd.mt6582.rc:root/ueventd.mt6582.rc \
-    device/digma/hit/rootdir/root/init.mt6582.usb.rc:root/init.mt6582.usb.rc \
+    $(LOCAL_PATH)/rootdir/fstab.mt6582:root/fstab.mt6582 \
+    $(LOCAL_PATH)/rootdir/init.mt6582.rc:root/init.mt6582.rc \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6582.rc:root/init.recovery.mt6582.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6582.usb.rc:root/init.mt6582.usb.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/init.charging.rc:root/init.charging.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/rootdir/ueventd.mt6582.rc:root/ueventd.mt6582.rc \
+    $(LOCAL_PATH)/rootdir/sbin/busybox:root/sbin/busybox \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
